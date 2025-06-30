820cd0724e0de1b4bc2f27f440e56ef935abd357
M.soliman <mahmoud.soliman@movement.college>
2025-06-30 16:50:16 +0300

    add OrderType.cs enum

---
+++ ExchangeExecutionPlanner/Models/OrderType.cs
@@ -0,0 +1,22 @@
+﻿namespace ExchangeExecutionPlanner.Models;
+
+public enum OrderType
+{
+    Buy,
+    Sell
+}
+
+public static class OrderTypeHelper
+{
+    public static OrderType Parse(string type)
+    {
+        return type.Equals("Buy", StringComparison.OrdinalIgnoreCase)
+            ? OrderType.Buy
+            : OrderType.Sell;
+    }
+
+    public static string ToString(OrderType type)
+    {
+        return type == OrderType.Buy ? "Buy" : "Sell";
+    }
+}
