f5b6d4b573e8b6f35f6e3897a5d39df3da070983
M.soliman <mahmoud.soliman@movement.college>
2025-06-30 14:11:17 +0300

    remove GetExecutionsPlans "N/A"

--- ExchangeExecutionPlanner/Services/ExchangeExecutionService.cs
+++ ExchangeExecutionPlanner/Services/ExchangeExecutionService.cs
@@ -14,15 +14,8 @@ public class ExchangeExecutionService : IExchangeExecutionService
 
     public async Task<ExecutionPlan> FindBestExecutionAsync(string orderType, decimal amount)
     {
-        var exchanges = await _repo.LoadAllExchangesAsync(); // Get data from repo (adjust args as needed)
-        // TODO: Logic to find the best execution plan
-        throw new NotImplementedException();
-    }
-
-    public async Task<List<ExecutionPlan>> GetExecutionPlansAsync(string orderType, decimal amount)
-    {
         var exchanges = await _repo.LoadAllExchangesAsync();
-        // TODO: Logic to get all execution plans
+        // TODO: Logic to find the best execution plan
         throw new NotImplementedException();
     }
 }
--- ExchangeExecutionPlanner/Services/IExchangeExecutionService.cs
+++ ExchangeExecutionPlanner/Services/IExchangeExecutionService.cs
@@ -5,5 +5,4 @@ namespace ExchangeExecutionPlanner.Services;
 public interface IExchangeExecutionService
 {
     Task<ExecutionPlan> FindBestExecutionAsync(string orderType, decimal amount);
-    Task<List<ExecutionPlan>> GetExecutionPlansAsync(string orderType, decimal amount);
 }
